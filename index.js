43bfcac4ee2c56d1ebf9565a7398c802bda664d6
abhiawatade <awatadeabhi@gmail.com>
2023-02-13 10:24:12 +0530

    feat : users can select password length

--- index.js
+++ index.js
@@ -1,19 +1,24 @@
 const characters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9","~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?",
 "/"];
 
+const  passGen = document.getElementById("pass") 
+const passOne = document.getElementById("pass-one")
+const passTwo = document.getElementById("pass-two")
+const passLength = document.getElementById("pass-length")
 
 
 function genratePassword (){
 let passwordOne = "";
-
-// let passwordlength = ("How long do you want your password to be?");
-for(let i = 0 ; i < 15;i++){
+let passwordlength = passLength.value;
+for(let i = 0 ; i < passwordlength;i++){
     let randomIndexOne = Math.floor(Math.random()*characters.length)
     passwordOne += characters[randomIndexOne]
-
 }
-console.log(passwordOne )
-
+return passwordOne;
 }
-genratePassword()
-genratePassword()
+
+passGen.addEventListener("click",function(){
+ 
+    passOne.textContent = genratePassword()
+    passTwo.textContent = genratePassword()
+})
