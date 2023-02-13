bdd83a8dd89934fbd14b71ae2e58bbf58d4a43d5
abhiawatade <awatadeabhi@gmail.com>
2023-02-13 12:52:22 +0530

    added styling && copy to clipboard feature

--- index.js
+++ index.js
@@ -1,24 +1,49 @@
-const characters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9","~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?",
-"/"];
+import characters from "./assets/chracters.js";
 
-const  passGen = document.getElementById("pass") 
-const passOne = document.getElementById("pass-one")
-const passTwo = document.getElementById("pass-two")
-const passLength = document.getElementById("pass-length")
+const passGen = document.getElementById("pass");
+const passOne = document.getElementById("pass-one");
+const passTwo = document.getElementById("pass-two");
+const passLength = document.getElementById("pass-length");
 
-
-function genratePassword (){
-let passwordOne = "";
-let passwordlength = passLength.value;
-for(let i = 0 ; i < passwordlength;i++){
-    let randomIndexOne = Math.floor(Math.random()*characters.length)
-    passwordOne += characters[randomIndexOne]
-}
-return passwordOne;
+function genratePassword() {
+  let passwordOne = "";
+  let passwordlength = passLength.value;
+  for (let i = 0; i < passwordlength; i++) {
+    let randomIndexOne = Math.floor(Math.random() * characters.length);
+    passwordOne += characters[randomIndexOne];
+  }
+  return passwordOne;
 }
 
-passGen.addEventListener("click",function(){
- 
-    passOne.textContent = genratePassword()
-    passTwo.textContent = genratePassword()
-})
+passGen.addEventListener("click", function () {
+  passOne.textContent = genratePassword();
+  passTwo.textContent = genratePassword();
+});
+
+passOne.addEventListener("click", function () {
+  //   passOne.select();
+  //   passOne.setSelectionRange(0, 99999);
+  //   navigator.clipboard.writeText(passOne.innerText);
+
+  //   const str = document.getElementById("pass-one").innerText;
+  //   const el = document.createElement("textarea");
+  //   el.value = str;
+  //   el.setAttribute("readonly", "");
+  //   document.body.appendChild(el);
+  //   el.select();
+  //   el.setSelectionRange(0, 99999);
+  //   document.execCommand("copy");
+  //   document.body.removeChild(el);
+
+  var copyText = document.getElementById("pass-one").innerText;
+  navigator.clipboard.writeText(copyText).then(() => {
+    alert("Copied to clipboard");
+  });
+});
+
+passTwo.addEventListener("click", function () {
+  var copyText = document.getElementById("pass-one").innerText;
+  navigator.clipboard.writeText(copyText).then(() => {
+    alert("Copied to clipboard");
+  });
+});
